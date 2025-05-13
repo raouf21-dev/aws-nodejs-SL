7d60a55798c51ae5f1b0b0c7730797746151b1b9
Raouf <raouf_dev@hotmail.com>
2025-05-13 18:17:03 +0100

    updated test

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin.env groovy
 
-def call(){
+def call(script){
         script.return env.BRANCH_NAME == "main"            
         script.echo "Testing pipeline..."
         script.echo "BRANCH_NAME: ${env.BRANCH_NAME}"
